fcc4fff51a5bb5297c1599f4d2e6d4db409931f6
higorrofino <higorrofino1@gmail.com>
2024-01-16 22:44:55 -0300

    Created classes and code is working

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2024;
+        job1._endYear = 2027;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Designer";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+
+        Resume res1 = new Resume();
+        res1._name = "Higor Rofino";
+        res1._jobs.Add(job1);
+        res1._jobs.Add(job2);
+        res1.Display();
     }
 }
